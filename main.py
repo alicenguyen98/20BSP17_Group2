348f7b3be542f1dc1a94be676931a7511ad244f3
Alice <haianh.ng98@gmail.com>
2021-05-04 18:43:31 +0100

    Added analyse entry point in main

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import data_collector
 import tweet_reviews
+import data_analyzer
 
 def main():
 
     # Get all the data from the past 6 days
-    for minus_days in range(7):
-        data_collector.get_new_tweets(items=2000, minus_days=minus_days)
-    
-    
-    # tweet_reviews.export('unreviewed.csv')
+    #for minus_days in range(7):
+    #   data_collector.get_new_tweets(items=2000, minus_days=minus_days)
+
+    #data_analyzer.sentiment_analysis()
     pass
 
 if  __name__ == "__main__":
